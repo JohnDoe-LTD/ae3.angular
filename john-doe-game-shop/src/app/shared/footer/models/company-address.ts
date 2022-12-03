405bbe3ff1fb467d038547dbb79778bfaa53f09c
Alfonso Segura <a.segura.gonzalez@gmail.com>
2022-12-03 16:08:42 +0100

    wip: company model

---
+++ john-doe-game-shop/src/app/shared/footer/models/company-address.ts
@@ -0,0 +1,11 @@
+export interface CompanyAddress {
+  companyName: string
+  address: string
+  city: string
+  state: string
+  zipCode: string
+  country: string
+  phone: string
+  email: string
+  website: string
+}
