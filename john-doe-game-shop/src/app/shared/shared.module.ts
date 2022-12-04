2d4275c51c4da88cb37cad2f1715c6393fe35797
Alfonso Segura <a.segura.gonzalez@gmail.com>
2022-12-04 19:56:19 +0100

    wip: complete shared module

--- john-doe-game-shop/src/app/shared/shared.module.ts
+++ john-doe-game-shop/src/app/shared/shared.module.ts
@@ -2,41 +2,40 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
-import { GameCardComponent } from './components/game-card/game-card.component';
-import { GamesTableComponent } from './components/games-table/games-table.component';
 import { HeaderComponent } from './header/header.component';
-import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { HeroComponent } from './hero/hero.component';
 import { FooterModule } from './footer/footer.module';
 import { FooterComponent } from './footer/components/footer/footer.component';
 import { AddressCardComponent } from './footer/components/address-card/address-card.component';
 import { ContentSectionComponent } from './content-section/content-section.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GamesTableComponent } from './games-table/games-table.component';
 
 @NgModule({
   declarations: [
     MenuComponent,
-    GameCardComponent,
-    GamesTableComponent,
     HeaderComponent,
-    UserDetailsComponent,
     HeroComponent,
-    ContentSectionComponent
+    ContentSectionComponent,
+    ContactFormComponent,
+    GamesTableComponent
   ],
   imports: [
     CommonModule,
     FooterModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
   ],
   exports: [
     MenuComponent,
-    GameCardComponent,
-    GamesTableComponent,
     HeaderComponent,
-    UserDetailsComponent,
     HeroComponent,
     FooterComponent,
     AddressCardComponent,
     ContentSectionComponent,
+    ContactFormComponent,
+    GamesTableComponent
   ]
 })
 export class SharedModule { }
