fab730a5d7fbad1a8e5f4d19b91b0b275fd7e451
Alfonso Segura <a.segura.gonzalez@gmail.com>
2022-12-04 19:55:35 +0100

    wip: change game service contract

--- john-doe-game-shop/src/app/core/services/game.service.ts
+++ john-doe-game-shop/src/app/core/services/game.service.ts
@@ -2,23 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Game } from '../models/game';
-import { GameDto } from '../models/game-dto';
-import { Link } from '../models/link';
+import { GamePage } from '../models/game-page';
 import { PageRequest } from '../models/page-request';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getGame(link: Link): Observable<Game> {
-    return this.http.get<Game>(link.href);
+  getGame(url: string): Observable<Game> {
+    return this.http.get<Game>(url);
   }
 
-  getGames(pageRequest: PageRequest): Observable<GameDto[]> {
-    return this.http.get<GameDto[]>(pageRequest.link.href);
+  getGames(pageRequest: PageRequest): Observable<GamePage> {
+    return this.http.get<GamePage>(pageRequest.link.href);
   }
-
 }
